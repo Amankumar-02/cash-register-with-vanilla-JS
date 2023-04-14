ebc735ddb015fd01076e168cd3cf6f52ea55554a
Aman <ak193566@gmail.com>
2023-04-15 01:23:48 +0530

    docs : update
    HTML & CSS part done

---
+++ script.js
@@ -0,0 +1,5 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-btn");
+
+// // billAmount.addEventListener()
